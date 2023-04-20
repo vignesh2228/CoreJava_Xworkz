baf649e5034b4acfd336f7d908614724d804ead2
vignesh2228 <kvignesh.xworkz@gmail.com>
2023-04-20 19:42:41 +0530

    the variavbles

--- Test.java
+++ Test.java
@@ -1,11 +1,36 @@
-class Main {
-    public static void main (String [] args ) {
-        int i;
-        for (i = 1; i <= 12 ; i += 2) {
-            if (i == 8 ) {
-                System.out.println(i);
-                break;
-            }
-        }
+class Test {
+    // static declaration 
+    static String name   ;
+    static int age ;
+    static long phoneNumber  ;
+    static boolean alive ; 
+    static char gender ;
+
+    public static void main (String [] args) {
+
+        name = "vicky";
+        age = 21 ;
+        phoneNumber = 6383772564l ;
+        alive = true ;
+        gender = 'M';
+        
+        System.out.println("The Person name is " + name );
+        System.out.println("The person age is " + age );
+        System.out.println("The phoneNumber is " + phoneNumber );
+        System.out.println("The Person Status " + alive);
+        System.out.println("the gender is " + gender );
+
+        System.out.println("");
+        
+
+        //String name = "karuna";
+        int age = 22 ;
+        long phone = 5445454545754l ;
+         
+        System.out.println("The Person name is " + name );
+        System.out.println("The Person age is " + age );
+        System.out.println("The phone number " + phone);
+
+
     }
 }
