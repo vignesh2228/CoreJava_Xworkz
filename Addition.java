bddda52c1212d9d68d0540bf51641f0fd7755756
vignesh2228 <kvignesh.xworkz@gmail.com>
2023-04-20 19:28:06 +0530

    the operators files

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class Addition {
+    public static void main (String [] args) {
+        System.out.println("The Main Started ");
+            multiple (450, 230 , 2);
+            multiple (448,554,52);
+            multiple(44,55,54);
+        System.out.println("The Main Ended");
+    }
+    public static void multiple (double a, double b, double c) {
+        System.out.println(a*b/c);
+    }
+}
