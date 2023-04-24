9aba6198dab4faa1e76fb4d35a280dbf33cb0dc9
vignesh2228 <kvignesh.xworkz@gmail.com>
2023-04-24 11:30:38 +0530

    the new files are updating to the veriables

---
+++ Oneplus_11.java
@@ -0,0 +1,21 @@
+class Oneplus_11 {
+    static String brandName = "    - onepluse";
+    static String modelName = "    - Onepluse 11R";
+    static String os = "   - OxygenOS";
+    static String memoryStorage = "    - 128 GB";
+    static String rom = "  - 8 GB + 4 GB virtual";
+    static String display = "  Amoled";
+    static int price = 39999;
+    public static void main (String [] args){
+        System.out.println("The OnePluse 11R 5G : ");
+        System.out.println("    ");
+        System.out.println("Brand" + brandName);
+        System.out.println("Model Name" +modelName);
+        System.out.println("Operating System" + os);
+        System.out.println("Memory Address" + memoryStorage);
+        System.out.println("Rom" +rom);
+        System.out.println("Display" + display);
+        System.out.println("");
+        System.out.println("The OnePlus phone price is  " + price);
+    }
+}
---
+++ Victus.java
@@ -0,0 +1,23 @@
+class Victus {
+    public static void main (String [] args) {
+        static String brand = "HP";
+        static String modelName = "HP Victus";
+        static String screenSize = "14.0 inch";
+        static String graphics = "Radon RX 6500M";
+        static String specialFeature = "Backlit Keyboard";
+        static int price = 59999;
+        static String processor = "Ryzon 5";
+        System.out.println("Victus Laptop AMD Ryzen 5 5600HGamming Laptop  Features:");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Brand" + brand) ;
+        System.out.println("Model Name" + modelName);
+        System.out.println("Screen Size" + screenSize);
+        System.out.println("Graphic Card"+ graphics);
+        System.out.println("Special Features" +specialFeature);
+        System.out.println("Processor" + processor);
+        System.out.println("");
+        System.out.println("The Hp Victus Ryzon 5 price is :" +price);
+
+    }
+}
